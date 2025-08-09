3652b5c90d827d3fe2602863052b488d81dacf80
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-08-09 12:56:04 +0300

    Update Stack.h

--- Implentation of Stack in C/Stack.h
+++ Implentation of Stack in C/Stack.h
@@ -2,30 +2,97 @@
 #define STACK_H_INCLUDED
 #define MAX 10
 
-typedef char typeEntry;
-
 typedef struct
 {
     int top;
-    typeEntry entry[MAX];
-}stackType;
+    int entry[MAX];
+}Stack;
 
-void creatStack(stackType *s);       // Create stack
 
-int stackIsEmpty(stackType s);      // Check weather is empty or not
+// Insertion function
+// Stack push function to insert at first
+void push(int data, Stack* s); 
 
-int push(typeEntry item, stackType *s);   // Push item into the stack array
+// Deletion function
+// Stack pop function to delete the first
+int pop(Stack* s);
 
-int pop(typeEntry *item, stackType *s);  // Pop item into the stack array
+// Stack function to clear all the stack
+void clear(Stack* s);
 
-int stack_size(stackType s);          // Returns stack size
 
-typeEntry stack_first(stackType s);        // returns the first element in the stack
+// Search function
+// Stack peek function to get the first element without deleting it
+int peek(Stack* s); 
 
-char last_element(stackType s);    // returns the last element
+// Stack getIndex function to get the index of specific element in stack
+int getIndex(int data, Stack* s); 
 
-typeEntry stackDestroy(stackType *s);     // destroy stack
+// Stack getElement function to get the element of specific index in stack
+int getElement(int index, Stack* s); 	
 
-typeEntry stackCopy(stackType *s, stackType *s1);    // copy stack
+// Stack getMiddle function to get the middle element
+int getMiddle(Stack* s); 	
+
+// Stack getLast function to get the last element
+int getLast(Stack* s); 
+
+// Stack isFound function to check if the specific element is found or not
+boolean isFound(int data, Stack* s);
+
+
+// Stats functions
+// Stack size function to get the size of the stack
+int size(Stack* s); 
+	
+// Stack sum function to get the sum of the stack
+int sum(Stack* s); 
+
+// Stack average function to get the average of the stack
+double average(Stack* s); 
+	
+// Stack repeated function to get the number of repeation of element in the stack
+int repeated(int data, Stack* s); 
+	
+// Stack max function to get the max num in the stack
+int max(Stack* s);
+	
+// Stack min function to get the min elemet in the stack
+int min(Stack* s);
+	
+
+//Utility functions
+// Stack printStack function to print all the stack element
+void printStack(Stack* s);
+	
+// Stack sort function to sort the stack using bubble sort algrothim
+void sort(Stack* s);
+  
+// Stack removeDuplicates function to remove all duplicates and works only when the list is "SORTED"
+void removeDuplicates(Stack* s);
+
+// Stack reverse function to reverse the Stack
+void reverse(Stack* s);
+
+// Stack swap function to swap data1 with data2
+void swapData(int data1, int data2, Stack* s);
+	
+// Stack swap function to swap index1 with index2
+void swapIndex(int index1, int index2, Stack* s);
+	
+
+// Extra Operations
+// Stack clone function to return clone of the stack
+Stack clone(Stack stack, Stack* s);
+	
+// Stack equals function to check if the stacks are equal or not
+boolean equals(Stack stack, Stack* s);
+
+// Stack concat function to append another stack to the current one
+Stack concat(Stack stack, Stack* s);
+
+// Stack isEmpty function to get if the stack is empty of full
+boolean isEmpty(Stack* s);
+
 
 #endif // STACK_H_INCLUDED
